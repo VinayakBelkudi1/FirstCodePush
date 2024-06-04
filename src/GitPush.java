27397a40a9d209730b4143192f94870a0ad73c11
VinayakBelkudi1 <vinayakbelkudi1997@gmail.com>
2024-06-04 19:40:21 +0530

    demo changes pushed

--- src/GitPush.java
+++ src/GitPush.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("Hi hi");
 	System.out.println("hello hello");
+	System.out.println("Changes in demo");
 }
 }
