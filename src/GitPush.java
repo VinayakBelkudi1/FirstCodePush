0182b6cba769d86bc72ba017e3ac6457c780fd31
VinayakBelkudi1 <vinayakbelkudi1997@gmail.com>
2024-06-04 20:03:04 +0530

    Pushing demo 123

--- src/GitPush.java
+++ src/GitPush.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("Hi hi");
 	System.out.println("hello hello");
-	System.out.println("Changes in demo");
+	System.out.println("Changedddd in demo");
 	System.out.println("Changed in Demo");
 }
 }
